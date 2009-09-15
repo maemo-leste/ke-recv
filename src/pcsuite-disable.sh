eec730b410dc0bc49318f9890541ef8ad81f2260
kihamala <kihamala@50eb543b-83fa-0310-9bb7-f7cbbee4ab39>
2009-09-15 13:09:33 +0000

    applied modified Felipe's patch
    git-svn-id: https://stage.maemo.org/svn/maemo/projects/haf/tags/ke-recv/3.19-11@19356 50eb543b-83fa-0310-9bb7-f7cbbee4ab39

--- src/pcsuite-disable.sh
+++ src/pcsuite-disable.sh
@@ -19,43 +19,6 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
 # 02110-1301 USA
 
-/sbin/lsmod | grep g_nokia > /dev/null
-if [ $? = 0 ]; then
-    logger "$0: removing g_nokia"
-
-    initctl emit G_NOKIA_REMOVE
-
-    PNATD_PID=`pidof pnatd`
-    if [ $? = 0 ]; then
-        kill $PNATD_PID
-    else
-        logger "$0: pnatd is not running"
-    fi
-    OBEXD_PID=`pidof obexd`
-    if [ $? = 0 ]; then
-        kill -HUP $OBEXD_PID
-    else
-        logger "$0: obexd is not running"
-    fi
-    SYNCD_PID=`pidof syncd`
-    if [ $? = 0 ]; then
-        kill $SYNCD_PID
-    else
-        logger "$0: syncd is not running"
-    fi
-
-    sleep 2
-    /sbin/rmmod g_nokia
-    if [ $? != 0 ]; then
-        logger "$0: failed to rmmod g_nokia!"
-        exit 1
-    fi
-
-    /sbin/modprobe g_file_storage stall=0 luns=2 removable
-    if [ $? != 0 ]; then
-        logger "$0: failed to install g_file_storage"
-        exit 1
-    fi
-fi
+logger "$0: do nothing to pass USB certs"
 
 exit 0
