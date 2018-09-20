134664ea1a86a44151181606ed8b2b48ab1bc796
Merlijn Wajer <merlijn@wizzup.org>
2018-09-20 13:38:37 +0200

    ke-recv.init: fix svc name

--- src/pcsuite-disable.sh
+++ src/pcsuite-disable.sh
@@ -19,29 +19,34 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
 # 02110-1301 USA
 
-if [ -e /sys/module/g_nokia ]; then
-    [ -f /etc/default/usbnetwork ] && . /etc/default/usbnetwork
-    if [ "$USBNETWORK_ENABLE" = "1" ]; then
-        logger "$0: disable USB network"
-        if [ "$USBNETWORK_DHCP" = "1" ]; then
-            if [ -f /var/run/dnsmasq.pid.usb0 ]; then
-                DNSMASQ_PID=`cat /var/run/dnsmasq.pid.usb0`
-                rm -f /var/run/dnsmasq.pid.usb0
-                kill $DNSMASQ_PID
-            else
-                logger "$0: dnsmasq for USB network is not running"
-            fi
-        fi
-        if [ "$USBNETWORK_NAT" = "1" ]; then
-            if [ -f /proc/sys/net/ipv4/ip_forward -a -x /usr/sbin/iptables ]; then
-                echo 0 > /proc/sys/net/ipv4/ip_forward
-                /usr/sbin/iptables -t nat -D POSTROUTING ! -o lo -j MASQUERADE
-            fi
-        fi
-        rm -f /var/run/resolv.conf.usb0
-        ifdown usb0
-        ifconfig usb0 down 0.0.0.0
-    fi
-fi
+logger "$0: removing pcsuite"
+
+ifconfig usb0 down
+/usr/sbin/hildon-usb-gadget-clear
+
+#if [ -e /sys/module/g_nokia ]; then
+#    [ -f /etc/default/usbnetwork ] && . /etc/default/usbnetwork
+#    if [ "$USBNETWORK_ENABLE" = "1" ]; then
+#        logger "$0: disable USB network"
+#        if [ "$USBNETWORK_DHCP" = "1" ]; then
+#            if [ -f /var/run/dnsmasq.pid.usb0 ]; then
+#                DNSMASQ_PID=`cat /var/run/dnsmasq.pid.usb0`
+#                rm -f /var/run/dnsmasq.pid.usb0
+#                kill $DNSMASQ_PID
+#            else
+#                logger "$0: dnsmasq for USB network is not running"
+#            fi
+#        fi
+#        if [ "$USBNETWORK_NAT" = "1" ]; then
+#            if [ -f /proc/sys/net/ipv4/ip_forward -a -x /usr/sbin/iptables ]; then
+#                echo 0 > /proc/sys/net/ipv4/ip_forward
+#                /usr/sbin/iptables -t nat -D POSTROUTING ! -o lo -j MASQUERADE
+#            fi
+#        fi
+#        rm -f /var/run/resolv.conf.usb0
+#        ifdown usb0
+#        ifconfig usb0 down 0.0.0.0
+#    fi
+#fi
 
 exit 0
